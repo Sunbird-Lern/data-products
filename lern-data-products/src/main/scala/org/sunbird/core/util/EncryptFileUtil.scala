cc02b5a5114f6dfeb69376960009cae7b154a8b3
Harikumar Palemkota <harip@ilimi.in>
2023-05-23 11:43:22 +0530

    LR-546 L4 level changes

--- lern-data-products/src/main/scala/org/sunbird/core/util/EncryptFileUtil.scala
+++ lern-data-products/src/main/scala/org/sunbird/core/util/EncryptFileUtil.scala
@@ -3,19 +3,20 @@ package org.sunbird.core.util
 import org.apache.spark.sql.SparkSession
 
 import javax.crypto.{Cipher, SecretKeyFactory}
-import javax.crypto.spec.{PBEKeySpec, SecretKeySpec}
+import javax.crypto.spec.{IvParameterSpec, PBEKeySpec, SecretKeySpec}
 import org.bouncycastle.util.io.pem.PemReader
 import org.ekstep.analytics.framework.Level.INFO
 import org.ekstep.analytics.framework.util.JobLogger
 import org.ekstep.analytics.framework.{FrameworkContext, StorageConfig}
 import org.sunbird.core.exhaust.JobRequest
 import org.sunbird.core.util.DataSecurityUtil.downloadCsv
+import org.sunbird.core.util.EncryptFileUtil2.{RSA_ALGORITHM, RSA_CBC_PKCS1, generateAESKey}
 
 import java.io.{File, FileOutputStream}
 import java.nio.ByteBuffer
 import java.nio.file.{Files, Paths}
 import java.security.SecureRandom
-import java.util.UUID
+import java.util.{Base64, UUID}
 
 object EncryptFileUtil extends Serializable {
 
@@ -32,6 +33,7 @@ object EncryptFileUtil extends Serializable {
         import java.security.spec.X509EncodedKeySpec
         var encryptedUUIDBytes: Array[Byte] = Array[Byte]()
         val encryptAESCipher : Cipher = Cipher.getInstance(AES_ALGORITHM)
+        var fileEncryptionKey: SecretKeySpec = null
         if(!"".equals(keyForEncryption))
         {
             //val userKey = new SecretKeySpec(keyForEncryption.getBytes, "AES")
@@ -45,32 +47,28 @@ object EncryptFileUtil extends Serializable {
             val keyFactory = KeyFactory.getInstance(RSA_ALGORITHM)
             val publicKeySpec = new X509EncodedKeySpec(pemObject.getContent)
             val publicKey = keyFactory.generatePublic(publicKeySpec)
-            val encryptRSACipher: Cipher = Cipher.getInstance(RSA_ALGORITHM)
+            fileEncryptionKey = generateAESKey(uuid.toString.toCharArray)
+            val encryptRSACipher: Cipher = Cipher.getInstance(RSA_CBC_PKCS1)
             encryptRSACipher.init(Cipher.ENCRYPT_MODE, publicKey)
-            encryptedUUIDBytes = encryptRSACipher.doFinal(uuid.toString.getBytes("UTF-8"))
+            encryptedUUIDBytes = encryptRSACipher.doFinal(fileEncryptionKey.getEncoded)
         }
-        val uuidBytes = new String(ByteBuffer.wrap(new Array[Byte](16))
-          .putLong(uuid.getMostSignificantBits)
-          .putLong(uuid.getLeastSignificantBits)
-          .array()).toCharArray
-        val key = generateAESKey(uuidBytes)
+//        val uuidBytes = new String(ByteBuffer.wrap(new Array[Byte](16))
+//          .putLong(uuid.getMostSignificantBits)
+//          .putLong(uuid.getLeastSignificantBits)
+//          .array()).toCharArray
+//        val key = generateAESKey(uuidBytes)
         val fileBytes = Files.readAllBytes(Paths.get(pathTuple._1))
-        encryptAESCipher.init(Cipher.ENCRYPT_MODE, key)
+        encryptAESCipher.init(Cipher.ENCRYPT_MODE, fileEncryptionKey, new IvParameterSpec(Array.fill[Byte](16)(0)))
         val encryptedAESContent = encryptAESCipher.doFinal(fileBytes)
-
         try {
-            val file = new File(pathTuple._1)
-            import java.io.BufferedWriter
-            import java.io.OutputStreamWriter
-            val bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"))
+            val file = new File(csvFilePath)
+            val fileWriter = new FileOutputStream(file)
             try {
-                bufferedWriter.write(level)
-                bufferedWriter.newLine()
-                bufferedWriter.write(new String(encryptedUUIDBytes))
-                bufferedWriter.newLine()
-                bufferedWriter.write(new String(encryptedAESContent))
+                fileWriter.write((level + "\n").getBytes())
+                fileWriter.write((Base64.getEncoder.encodeToString(encryptedUUIDBytes) + "\n").getBytes())
+                fileWriter.write(encryptedAESContent)
             }
-            finally if (bufferedWriter != null) bufferedWriter.close()
+            finally if (fileWriter != null) fileWriter.close()
         }
     }
 
