48c55b6b6972019eae8404bf883dfc53b16cc55e
Harikumar Palemkota <harip@ilimi.in>
2024-01-30 12:49:35 +0530

    Added config to getStorageConfig method

--- lern-data-products/src/main/scala/org/sunbird/userorg/job/report/StateAdminReportJob.scala
+++ lern-data-products/src/main/scala/org/sunbird/userorg/job/report/StateAdminReportJob.scala
@@ -8,7 +8,7 @@ import org.apache.spark.sql._
 import org.ekstep.analytics.framework.Level.{ERROR, INFO}
 import org.ekstep.analytics.framework.util.DatasetUtil.extensions
 import org.ekstep.analytics.framework.util.{JSONUtils, JobLogger}
-import org.ekstep.analytics.framework.{FrameworkContext, IJob, JobConfig, JobContext}
+import org.ekstep.analytics.framework.{FrameworkContext, IJob, JobConfig, JobContext, StorageConfig}
 import org.sunbird.core.util.DecryptUtil
 import org.sunbird.cloud.storage.conf.AppConf
 import org.sunbird.core.util.DataSecurityUtil.{getSecuredExhaustFile, getSecurityLevel, zipAndPasswordProtect}
@@ -26,7 +26,7 @@ object StateAdminReportJob extends IJob with StateAdminReportHelper {
     
     val container = AppConf.getConfig("cloud.container.reports")
     val objectKey = AppConf.getConfig("admin.metrics.cloud.objectKey")
-    val storageConfig = getStorageConfig(container, objectKey);
+    var storageConfig: StorageConfig = _
     //$COVERAGE-OFF$ Disabling scoverage for main and execute method
     def name(): String = "StateAdminReportJob"
 
@@ -35,6 +35,7 @@ object StateAdminReportJob extends IJob with StateAdminReportHelper {
         JobLogger.init(name())
         JobLogger.start("Started executing", Option(Map("config" -> config, "model" -> name)))
         val jobConfig = JSONUtils.deserialize[JobConfig](config)
+        storageConfig = getStorageConfig(container, objectKey, jobConfig);
         JobContext.parallelization = 10
         implicit val sparkSession: SparkSession = openSparkSession(jobConfig);
         implicit val frameworkContext = getReportingFrameworkContext();
